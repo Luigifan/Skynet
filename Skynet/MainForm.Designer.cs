daeb0f7122155115489c6cf381ba0167814ec707
Mike Santiago <miketheripper1@gmail.com>
2015-10-08 20:51:54 -0700

    Remove glass

--- Skynet/MainForm.Designer.cs
+++ Skynet/MainForm.Designer.cs
@@ -36,49 +36,89 @@ namespace Skynet
 		/// </summary>
 		private void InitializeComponent()
 		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.viewportPictureBox = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.updateInterval = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
             ((System.ComponentModel.ISupportInitialize)(this.viewportPictureBox)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // viewportPictureBox
             // 
             this.viewportPictureBox.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.viewportPictureBox.Location = new System.Drawing.Point(0, 89);
+            this.viewportPictureBox.Location = new System.Drawing.Point(0, 28);
             this.viewportPictureBox.Name = "viewportPictureBox";
-            this.viewportPictureBox.Size = new System.Drawing.Size(772, 383);
+            this.viewportPictureBox.Size = new System.Drawing.Size(772, 444);
             this.viewportPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.viewportPictureBox.TabIndex = 2;
             this.viewportPictureBox.TabStop = false;
             // 
-            // button1
+            // toolStrip1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 60);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(748, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripLabel1,
+            this.updateInterval});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(772, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
             // 
-            // panel1
+            // toolStripButton1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(2)))), ((int)(((byte)(3)))));
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(2)))), ((int)(((byte)(3)))));
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.MaximumSize = new System.Drawing.Size(0, 32);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(772, 32);
-            this.panel1.TabIndex = 4;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
+            // updateInterval
+            // 
+            this.updateInterval.Name = "updateInterval";
+            this.updateInterval.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripLabel1.Image")));
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(143, 22);
+            this.toolStripLabel1.Text = "Update Interval (in ms)";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(772, 472);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.viewportPictureBox);
             this.Name = "MainForm";
             this.Text = "Skynet - View of ";
@@ -86,13 +126,21 @@ namespace Skynet
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResizeEnd += new System.EventHandler(this.MainForm_ResizeEnd);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.viewportPictureBox)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         private System.Windows.Forms.PictureBox viewportPictureBox;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox updateInterval;
     }
 }
--- Skynet/MainForm.cs
+++ Skynet/MainForm.cs
@@ -24,139 +24,6 @@ namespace Skynet
         public event OnConnectionEstablished Connected;
 
 
-        #region ugly
-
-        #region Definitions
-        private MARGINS margins = new MARGINS();
-        private Rectangle topRect = new Rectangle();
-        private Rectangle leftRect = new Rectangle();
-        private Rectangle rightRect = new Rectangle();
-        private Rectangle botRect = new Rectangle();
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MARGINS
-        {
-            public int Left;
-            public int Right;
-            public int Top;
-            public int Bottom;
-        }
-        [DllImport("dwmapi.dll", PreserveSig = false)]
-        public static extern void DwmExtendFrameIntoClientArea(IntPtr hwnd, ref MARGINS margins);
-
-        [DllImport("dwmapi.dll", PreserveSig = false)]
-        public static extern bool DwmIsCompositionEnabled();
-        #endregion
-
-        #region Overrides
-        protected override void WndProc(ref Message m)
-        {
-            base.WndProc(ref m);
-            if (m.Msg == VistaApi.WM_NCHITTEST // if this is a click
-              && m.Result.ToInt32() == VistaApi.HTCLIENT // ...and it is on the client
-              && this.IsOnGlass(m.LParam.ToInt32())) // ...and specifically in the glass area
-            {
-                m.Result = new IntPtr(VistaApi.HTCAPTION); // lie and say they clicked on the title bar
-            }
-            //if we were still interested in having glass portions move, sure this would be great
-            const int WM_SYSCOMMAND = 0x0112;
-            const int SC_MOVE = 0xF010;
-
-            switch (m.Msg)
-            {
-                case WM_SYSCOMMAND:
-                    int command = m.WParam.ToInt32() & 0xfff0;
-                    if (command == SC_MOVE)
-                        return;
-                    break;
-            }
-
-            //base.WndProc(ref m);
-        }
-
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-            if (DwmIsCompositionEnabled())
-            {
-                //Rectangle together = new Rectangle(imageGroupBox.Location.X, imageGroupBox.Location.Y,
-                //    imageGroupBox.Width + maskGroupBox.Width, imageGroupBox.Height + resultGroupBox.Height + saveResultButton.Height);
-                margins = new MARGINS();
-                margins.Top = 38; //(this.Height) / 2 / 2 / 2;
-                margins.Left = 0;//this.ClientRectangle.Left; //(this.Width) / 2 / 2 / 2;
-                margins.Right = 0;//this.ClientRectangle.Right;
-                margins.Bottom = 0;//this.ClientRectangle.Bottom;
-                //Define rectnangles, for detection of whether or not the selected area is glass
-                topRect = new Rectangle(0, 0, ClientSize.Width, margins.Top);
-                //leftRect = new Rectangle(0, 0, (ClientSize.Width) / 2, margins.Top);
-                //rightRect = new Rectangle(0, 0, (ClientSize.Width) / 2, margins.Right);
-                //botRect = new Rectangle(0, 0, ClientSize.Width, margins.Top);
-                //
-                DwmExtendFrameIntoClientArea(this.Handle, ref margins);
-            }
-        }
-        protected override void OnPaintBackground(PaintEventArgs e)
-        {
-            base.OnPaintBackground(e);
-            if (DwmIsCompositionEnabled())
-            {
-                this.Update();
-                e.Graphics.Clear(Color.Black);
-                Rectangle clientArea = new Rectangle(
-                    margins.Left,
-                    margins.Top,
-                    this.ClientRectangle.Width - margins.Left - margins.Right,
-                    this.ClientRectangle.Height - margins.Top - margins.Bottom
-                    );
-                Brush b = new SolidBrush(this.BackColor);
-                e.Graphics.FillRectangle(b, clientArea);
-                this.Update();
-            }
-        }
-        #endregion
-
-        #region other stuffs
-        private bool IsGlassEnabled()
-        {
-            if (Environment.OSVersion.Version.Major < 6)
-            {
-                Debug.WriteLine("Not drawing glass");
-                return false;
-            }
-
-            //Check if DWM is enabled
-            bool isGlassSupported = false;
-            VistaApi.DwmIsCompositionEnabled(ref isGlassSupported);
-            return isGlassSupported;
-        }
-
-        private bool IsOnGlass(int lParam)
-        {
-            // sanity check
-            if (!this.IsGlassEnabled())
-            {
-                return false;
-            }
-
-            // get screen coordinates
-            int x = (lParam << 16) >> 16; // lo order word
-            int y = lParam >> 16; // hi order word
-
-            // translate screen coordinates to client area
-            Point p = this.PointToClient(new Point(x, y));
-
-            // work out if point clicked is on glass
-            if (topRect.Contains(p) || leftRect.Contains(p) || rightRect.Contains(p) || botRect.Contains(p))
-            {
-                return true;
-            }
-
-            return false;
-        }
-        #endregion
-
-        #endregion
-
         public MainForm(string address)
         {
 
@@ -235,6 +102,8 @@ namespace Skynet
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
+            if (ws == null)
+                Environment.Exit(0);
             if (ws.IsAlive)
             {
                 DialogResult dr = MessageBox.Show("A connection is still active, are you sure you want to quit?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
@@ -245,6 +114,8 @@ namespace Skynet
                     Environment.Exit(0);
                 }
             }
+            else
+                Environment.Exit(0);
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -254,24 +125,13 @@ namespace Skynet
 
         private void MainForm_ResizeEnd(object sender, EventArgs e)
         {
-            if (DwmIsCompositionEnabled())
-            {
-                //Rectangle together = new Rectangle(imageGroupBox.Location.X, imageGroupBox.Location.Y,
-                //    imageGroupBox.Width + maskGroupBox.Width, imageGroupBox.Height + resultGroupBox.Height + saveResultButton.Height);
-                margins = new MARGINS();
-                margins.Top = 60; //(this.Height) / 2 / 2 / 2;
-                margins.Left = 0;//this.ClientRectangle.Left; //(this.Width) / 2 / 2 / 2;
-                margins.Right = 0;//this.ClientRectangle.Right;
-                margins.Bottom = 0;//this.ClientRectangle.Bottom;
-                //Define rectnangles, for detection of whether or not the selected area is glass
-                topRect = new Rectangle(0, 0, ClientSize.Width, margins.Top);
-                //leftRect = new Rectangle(0, 0, (ClientSize.Width) / 2, margins.Top);
-                //rightRect = new Rectangle(0, 0, (ClientSize.Width) / 2, margins.Right);
-                //botRect = new Rectangle(0, 0, ClientSize.Width, margins.Top);
-                //
-                DwmExtendFrameIntoClientArea(this.Handle, ref margins);
-            }
+            
+        }
 
+        FormWindowState LastWindowState = FormWindowState.Normal;
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            
         }
     }
 }
